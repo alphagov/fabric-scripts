73347a3847bda56550c8c565666f05664f864cb1
Nick Stenning <nick@whiteink.com>
2012-11-30 15:06:35 +0000

    Don't worry if grep doesn't match

--- licensify.py
+++ licensify.py
@@ -4,7 +4,7 @@ from fabric.api import *
 @roles('class-licensify-frontend', 'class-licensify-backend')
 def greplogs(pattern, context=100):
     "Grep the logs for a pattern"
-    grep_cmd = "[ -f /var/apps/{app}/logs/application.log ] && egrep '{pattern}' -C{context} /var/apps/{app}/logs/application.log"
+    grep_cmd = "[ -f /var/apps/{app}/logs/application.log ] && egrep '{pattern}' -C{context} /var/apps/{app}/logs/application.log || :"
     run(grep_cmd.format(app='licensify',       pattern=pattern, context=context))
     run(grep_cmd.format(app='licensify-admin', pattern=pattern, context=context))
     run(grep_cmd.format(app='licensify-feed',  pattern=pattern, context=context))
