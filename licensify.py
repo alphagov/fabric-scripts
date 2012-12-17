6d4d9a7ff0b6c6d70f8c6ca03e2701d325c49ddf
Nick Stenning <nick@whiteink.com>
2012-12-17 12:10:58 +0000

    Search old logs too

--- licensify.py
+++ licensify.py
@@ -4,7 +4,7 @@ from fabric.api import *
 @roles('class-licensify-frontend', 'class-licensify-backend')
 def greplogs(pattern, context=100):
     "Grep the logs for a pattern"
-    grep_cmd = "egrep '{pattern}' -C{context} /var/log/{app}/* --include='*.log' || :"
+    grep_cmd = "zgrep -E '{pattern}' -C{context} /var/log/{app}/*{{.log,.gz}} 2>/dev/null || true"
     run(grep_cmd.format(app='licensify',       pattern=pattern, context=context))
     run(grep_cmd.format(app='licensify-admin', pattern=pattern, context=context))
     run(grep_cmd.format(app='licensify-feed',  pattern=pattern, context=context))
