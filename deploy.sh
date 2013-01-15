f19c39e878c3349c7d8ac1e2bfd9a7e31e1a1695
Sam J Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2013-01-15 15:01:11 +0000

    SSH known hosts format has changed

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -eu
 
-DEPLOY_TO="$(grep -E 'jumpbox-[0-9]+' /etc/ssh/ssh_known_hosts | awk '{ print $1 }' | xargs)"
+DEPLOY_TO="$(grep -E 'jumpbox-[0-9]+' /etc/ssh/ssh_known_hosts | awk '{ print $1 }' | cut -d, -f1 | xargs)"
 
 for server in $DEPLOY_TO; do
   rsync -av --delete --exclude='.git' "$(pwd)/" "deploy@${server}":/usr/local/share/govuk-fabric/
