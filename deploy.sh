768960be3da40c4930a721769d3052f3006ae400
Sam J Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2013-03-11 14:20:21 +0000

    Log to syslog when fabric is deployed

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,5 @@ set -eu
 
 govuk_node_list -c jumpbox | while read server; do
   rsync -av --delete --exclude='.git' "$(pwd)/" "deploy@${server}":/usr/local/share/govuk-fabric/
-done 
+done
+logger -p INFO -t jenkins "DEPLOYMENT: ${JOB_NAME} ${BUILD_NUMBER} (${BUILD_URL})"
