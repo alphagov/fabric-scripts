4738305131d58b9778b4864d1a25c7017dceeb7d
Dan Carley <dan.carley@gmail.com>
2013-06-04 09:09:52 +0100

    Wait for Nginx to stop before returning
    Make the `nginx.gracefulstop` task poll for the Nginx process until it has
    exited, by default. This makes it safe to use as such:
    
        fab preview class:frontend nginx.gracefulstop sdo:reboot
    
    This behaviour can be disabled by passing a boolean argument:
    
        fab preview class:frontend nginx.gracefulstop:wait=False

--- nginx.py
+++ nginx.py
@@ -1,6 +1,10 @@
 from fabric.api import *
 
 @task
-def gracefulstop(*args):
+def gracefulstop(wait=True):
     """Gracefully shutdown Nginx by finishing any in-flight requests"""
     sudo('nginx -s quit')
+
+    if wait:
+        # Poll for Nginx, until it's no longer running.
+        run('while pgrep nginx >/dev/null; do echo "Waiting for Nginx to exit.."; sleep 1; done')
