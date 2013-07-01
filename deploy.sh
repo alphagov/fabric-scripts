7e0d73a556fe77437b86650b88c64b264f96f67c
Dan Carley <dan.carley@gmail.com>
2013-07-01 14:00:14 +0100

    [#52532037] Remove deploy.sh
    Since we no longer deploy this. It should only be used locally.

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -eu
-
-govuk_node_list -c jumpbox | while read server; do
-  rsync -av --delete --exclude='.git' "$(pwd)/" "deploy@${server}":/usr/local/share/govuk-fabric/
-done
-logger -p INFO -t jenkins "DEPLOYMENT: ${JOB_NAME} ${BUILD_NUMBER} (${BUILD_URL})"
