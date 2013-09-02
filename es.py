88b729800b565e1f3177cb9a17ec82f4a420f473
bob walker <bob.walker@digital.cabinet-office.gov.uk>
2013-09-02 16:33:35 +0100

    Add the ability to delete an index.
    We at times need to delete the _river index so that puppet can recreate it and the logs can flow again.
    
    Also added status command.

---
+++ es.py
@@ -0,0 +1,11 @@
+from fabric.api import *
+
+@task
+def delete(index):
+    """Delete an index"""
+    run("curl -XDELETE 'http://localhost:9200/%s'" % index)
+
+@task
+def status(index):
+    """Get the status of an index"""
+    run("curl -XGET 'http://localhost:9200/%s/_status'" % index)
