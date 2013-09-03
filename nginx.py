fb34a738ebb35bceed7d9e2f9aad1db356f508fb
Sam J Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2013-09-03 14:45:43 +0100

    Add tasks to start and restart nginx

--- nginx.py
+++ nginx.py
@@ -8,3 +8,14 @@ def gracefulstop(wait=True):
     if wait:
         # Poll for Nginx, until it's no longer running.
         run('while pgrep nginx >/dev/null; do echo "Waiting for Nginx to exit.."; sleep 1; done')
+
+@task
+def gracefulstart():
+    """Start up Nginx on a machine"""
+    sudo('service nginx start')
+
+@task
+def hello_it():
+    """Turns Nginx off and on again"""
+    gracefulstop()
+    gracefulstart()
