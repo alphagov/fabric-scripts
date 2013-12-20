6b9a75a25b5d5a11d46f942cc91e4680c5179789
Bradley Wright <bradley.wright@digital.cabinet-office.gov.uk>
2013-12-20 15:56:46 +0000

    Abstract template rendering out
    This allows us to print the contents of the template once, ensuring we
    can use it instructionally.

--- campaigns.py
+++ campaigns.py
@@ -15,33 +15,38 @@ def validate_classes(campaign_class):
     raise Exception, "Invalid class %s, valid values are 'red', 'black', 'green'" % campaign_class
 
 
-@task
 @runs_once
-def homepage_env():
+def homepage_template():
     """Promotes a campaign to the homepage of GOV.UK"""
-    prompt("Heading for campaign:", key='homepage_heading')
-    prompt("Extra information for campaign:", key='homepage_extra_info')
-    prompt("Link for more information:", key='homepage_more_info')
-    prompt("Campaign class:", key='homepage_campaign_class', validate=validate_classes)
+    context = {
+        'heading': prompt("Heading for campaign:"),
+        'extra_info': prompt("Extra information for campaign:"),
+        'more_info': prompt("Link for more information:"),
+        'campaign_class': prompt("Campaign class:", validate=validate_classes)
+    }
+
+    template = Template("""<div id="campaign" class="{{ campaign_class }}">
+  <div class="campaign-inner">
+    <h1>{{ heading|e }}</h1>
+    <p>{{ extra_info|e }}</p>
+    <a href="{{ more_info|e }}">More information</a>
+  </div>
+</div>""")
+    env['template_contents'] = template.render(context)
+
+    print "Template contents:\n%s" % env['template_contents']
 
 @task
 @roles('class-frontend')
 def deploy_to_homepage():
-    execute(homepage_env)
-    template = Template("""<div id="campaign" class="{{ homepage_campaign_class }}">
-  <div class="campaign-inner">
-    <h1>{{ homepage_heading|e }}</h1>
-    <p>{{ homepage_extra_info|e }}</p>
-    <a href="{{ homepage_more_info|e }}">More information</a>
-  </div>
-</div>""")
-    contents = template.render(env)
+    execute(homepage_template)
+
+    contents = env['template_contents']
 
     remote_filename = '/var/apps/frontend/app/views/root/_campaign_notification.html.erb'
     put(StringIO.StringIO(contents), remote_filename, use_sudo=True, mirror_local_mode=True)
     sudo('chown deploy:deploy %s' % remote_filename)
     execute(reload_unicorn, name='frontend')
-    print "Campaign deployed. Please ensure this is copied into the frontend repository to ensure it's stable across deploys"
 
 @task
 @roles('class-frontend')
