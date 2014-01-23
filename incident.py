3cd8abda7d790d9501c82cd13989991005465c4c
Philip Potter <philip.potter@digital.cabinet-office.gov.uk>
2014-01-23 14:25:43 +0000

    reword incident response output

--- incident.py
+++ incident.py
@@ -9,4 +9,4 @@ def fail_to_mirror():
     puppet.disable()
     nginx.disable_vhost("www.gov.uk")
     nginx.hello_it()
-    print("Disabled Puppet and disabled www.gov.uk vhost, remember to re-enable these")
+    print("Disabled Puppet and www.gov.uk vhost, remember to re-enable and re-run puppet to restore previous state")
