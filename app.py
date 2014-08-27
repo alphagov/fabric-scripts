03f5422c26dde376237c513ae4bb5d216c997eb3
Dan Carley <dan.carley@gmail.com>
2014-08-27 09:56:04 +0100

    Add app.reload task
    Which reloads an app gracefully. Ruby apps which use unicornherder while
    start a new set of workers and transition over to them within a couple of
    minutes.

--- app.py
+++ app.py
@@ -2,5 +2,10 @@ from fabric.api import *
 
 @task
 def restart(app):
-    """Restart a particular app"""
+    """Restart (hard) a particular app"""
     sudo("service %s restart" % app)
+
+@task
+def reload(app):
+    """Reload (graceful) a particular app"""
+    sudo("service %s reload" % app)
