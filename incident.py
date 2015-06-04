3e933bd9b58f2ebe3a77c2d9993ababa3c2ca26c
Matt Bostock <matt@mattbostock.com>
2015-06-04 15:37:39 +0100

    Add a reason when disabling Puppet
    Otherwise, `puppet.disable()` will fail.

--- incident.py
+++ incident.py
@@ -6,7 +6,7 @@ import puppet
 @roles('class-cache')
 def fail_to_mirror():
     """Fails the site to the mirror"""
-    puppet.disable()
+    puppet.disable("Fabric fail_to_mirror task invoked")
     nginx.disable_vhost("www.gov.uk")
     nginx.hello_it()
     print("Disabled Puppet and www.gov.uk vhost, remember to re-enable and re-run puppet to restore previous state")
