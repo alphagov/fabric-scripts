5b566754cf112db6f349fcb363253d4be303d50b
Matt Bostock <matt@mattbostock.com>
2015-06-05 09:33:11 +0100

    Rename nginx.hello_it to nginx.force_restart
    While I'm a fan of The IT Crowd, cultural references aren't useful as
    descriptive method names.
    
    It also excludes anyone not familiar with the brilliance of [Street
    Countdown](https://www.youtube.com/watch?v=kYj1Gb3qQc0).

--- incident.py
+++ incident.py
@@ -8,5 +8,5 @@ def fail_to_mirror():
     """Fails the site to the mirror"""
     puppet.disable("Fabric fail_to_mirror task invoked")
     nginx.disable_vhost("www.gov.uk")
-    nginx.hello_it()
+    nginx.force_restart()
     print("Disabled Puppet and www.gov.uk vhost, remember to re-enable and re-run puppet to restore previous state")
