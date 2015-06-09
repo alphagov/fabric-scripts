fff1ea832241fc4ee6a8c071fda3d6d9ed224f40
Timothy Mower <timothy.mower@gmail.com>
2015-06-09 11:18:52 +0100

    Add a postgres sync command
    This relies on agent forwarding to save copying a temporary file down to
    where the script is running and back up again. This is temporarily
    enabled with a fabric command switch.
    
    This runs pg_dump to dump the database on the source machine and pipes
    it to postgres running on the destination machine - as the two sides of
    the pipeline run as different users fabrics sudo decorator cannot be
    used. pg_restore is used to restore the dump file as this is more
    robust when syncing between different postgres versions

---
+++ postgresql.py
@@ -0,0 +1,16 @@
+from fabric.api import task, sudo, env, settings, run
+from fabric.utils import abort
+
+@task
+def sync(database, dest_machine):
+    # the agent forwarding is required for the ssh command to the destination
+    # postgres server to work - this saves copying a temporary file down and
+    # back up again. We cant use fabrics sudo context manager because the
+    # command after the pipe should not run as the postgres user.
+    # The combination of "--schema public" and "--clean" in the pg_restore
+    # command clears all existing tables in the public schema before restoring
+    # the tables and data
+    with settings(forward_agent=True):
+        run('sudo -iupostgres pg_dump -Fc {0} | ssh {1} '
+            '"sudo -upostgres pg_restore --clean --single-transaction '
+            '--schema public --dbname {0}"'.format(database, dest_machine))
