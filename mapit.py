b724a96142848c8f51e573de306cc7336603cbf0
Alex Muller <alex.muller@digital.cabinet-office.gov.uk>
2015-06-19 12:51:00 +0100

    Add script to test a Mapit database upgrade
    We replay Mapit data to ensure that the upgrade works as we expect.

--- mapit.py
+++ mapit.py
@@ -1,4 +1,4 @@
-from fabric.api import task, settings, sudo, env
+from fabric.api import task, settings, sudo, env, run
 from fabric.tasks import execute
 
 import app
@@ -22,3 +22,18 @@ def update_database():
       sudo("psql -c 'DROP DATABASE mapit;'")
 
     execute(puppet.agent, '--test')
+
+
+@task
+def check_database_upgrade():
+    """Replay yesterday's Mapit requests to ensure that a database upgrade works"""
+
+    sudo("awk '$9==200 {print \"http://localhost\" $7}' /var/log/nginx/mapit.access.log.1 > mapit-200s")
+    sudo("awk '$9==404 {print \"http://localhost\" $7}' /var/log/nginx/mapit.access.log.1 > mapit-404s")
+
+    print "Replaying Mapit 200s. Ensure that they are all still 200s."
+    run('while read line; do curl -sI $line | grep HTTP/1.1 ; done < mapit-200s | sort | uniq -c')
+    print "Replaying Mapit 404s. Ensure that they are all either 200s or 404s."
+    run('while read line; do curl -sI $line | grep HTTP/1.1 ; done < mapit-404s | sort | uniq -c')
+
+    sudo('rm ~/mapit-200s ~/mapit-404s')
