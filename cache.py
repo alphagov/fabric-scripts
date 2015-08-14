9b88184ed5076f14e45fd752cff729908c2883e2
David Heath <david.heath@digital.cabinet-office.gov.uk>
2015-08-14 10:20:56 +0100

    Make varnish cache purge tasks purge draft caches
    modify these tasks to purge the draft caches as well as the live ones.
    
    I couldn't think of a scenario in which it would be important to only
    purge one set of caches and not the other, so I've just made it default
    to purging both live and draft.

--- cache.py
+++ cache.py
@@ -1,14 +1,14 @@
 from fabric.api import *
 
 @task
-@roles('class-cache')
+@roles('class-cache', 'class-draft_cache')
 def purge(*args):
     "Purge items from varnish, eg \"/one,/two,/three\""
     for path in args:
         run("curl -s -I -X PURGE http://localhost:7999%s | grep '200 Purged'" % path.strip())
 
 @task
-@roles('class-cache')
+@roles('class-cache', 'class-draft_cache')
 def restart():
     """
     Restart Varnish caches
@@ -16,7 +16,7 @@ def restart():
     sudo('/etc/init.d/varnish restart')
 
 @task(default=True)
-@roles('class-cache')
+@roles('class-cache', 'class-draft_cache')
 def stats():
     "Show details about varnish performance"
     sudo('varnishstat -1')
