40e8afd19d82a8fb02cdd1d2848740ab86296d26
Josh Myers <joshuajmyers@gmail.com>
2015-08-17 15:38:35 +0100

    Automate emergency publishing to frontend + static
    To publish emergency content we need frontend + static
    applications checked out locally, make changes, commit,
    wait for CI to build, merge, deploy etc.
    
    In the case of an emergency this is going to take too
    long.
    
    This task builds upon prior art to automate all of the
    steps in the opsmanual (soon to be updated).
    
    See opsmanual for more info on the available tasks.

--- campaigns.py
+++ campaigns.py
@@ -6,52 +6,89 @@ from fabric.tasks import execute
 
 from jinja2 import Template
 
-import vm
+import app
+
+
+env['eagerly_disconnect'] = True
+
+
+APPLICATIONS = ['frontend', 'static']
+CAMPAIGN_CLASSES = ['red', 'black', 'green']
+
 
 def validate_classes(campaign_class):
     """Checks that the campaign class is valid"""
-    if campaign_class in ['red', 'black', 'green']:
+    if campaign_class in CAMPAIGN_CLASSES:
         return campaign_class
-    raise Exception, "Invalid class %s, valid values are 'red', 'black', 'green'" % campaign_class
+    raise Exception, "Invalid class {}, valid values are {}".format(campaign_class, CAMPAIGN_CLASSES)
 
 
 @runs_once
-def homepage_template():
-    """Promotes a campaign to the homepage of GOV.UK"""
-    context = {
-        'heading': prompt("Heading for campaign:"),
-        'extra_info': prompt("Extra information for campaign:"),
-        'more_info': prompt("Link for more information:"),
-        'campaign_class': prompt("Campaign class:", validate=validate_classes)
+def set_context():
+    env['context'] = {
+        'heading': prompt("Heading for campaign:", 'heading'),
+        'extra_info': prompt("Extra information for campaign:", 'extra_info'),
+        'more_info': prompt("Link for more information:", 'more_info'),
+        'campaign_class': prompt("Campaign class:", 'campaign_class', validate=validate_classes)
     }
 
-    template = Template("""<div id="campaign" class="{{ campaign_class }}">
-  <div class="campaign-inner">
-    <h1>{{ heading|e }}</h1>
-    <p>{{ extra_info|e }}</p>
-    <a href="{{ more_info|e }}">More information</a>
-  </div>
-</div>""")
-    env['template_contents'] = template.render(context)
 
-    print "Template contents:\n%s" % env['template_contents']
+def template(app):
+    if app == 'frontend':
+      template = Template("""<div id="campaign" class="{{ campaign_class }}">
+    <div class="campaign-inner">
+      <h1>{{ heading|e }}</h1>
+      <p>{{ extra_info|e }}</p>
+      <a href="{{ more_info|e }}">More information</a>
+    </div>
+  </div>""")
+    elif app == 'static':
+      template = Template("""<p>{{ heading|e }}<br />
+    {{ extra_info|e }}</p>
+  <a href="#" class="right">{{ more_info|e }}</a>""")
 
-@task
-@roles('class-frontend')
-def deploy_to_homepage():
-    execute(homepage_template)
+    env['template_contents'] = template.render(env.context)
 
-    contents = env['template_contents']
-
-    remote_filename = '/var/apps/frontend/app/views/root/_campaign_notification.html.erb'
-    put(StringIO.StringIO(contents), remote_filename, use_sudo=True, mirror_local_mode=True)
-    sudo('chown deploy:deploy %s' % remote_filename)
-    execute(vm.reload_unicorn, name='frontend')
 
 @task
 @roles('class-frontend')
-def remove_from_homepage():
-    remote_filename = '/var/apps/frontend/app/views/root/_campaign_notification.html.erb'
-    put(StringIO.StringIO(''), remote_filename, use_sudo=True, mirror_local_mode=True)
+def deploy_banner(application):
+    execute(template, application)
+    if application == 'frontend':
+        remote_filename = '/var/apps/frontend/app/views/root/_campaign_notification.html.erb'
+    elif application == 'static':
+        remote_filename = "/var/apps/static/app/views/notifications/banner_{}.erb".format(env.campaign_class)
+    content = env['template_contents']
+    put(StringIO.StringIO(content), remote_filename, use_sudo=True, mirror_local_mode=True)
     sudo('chown deploy:deploy %s' % remote_filename)
-    execute(vm.reload_unicorn, name='frontend')
+    execute(app.reload, application)
+
+def remove_banner(application):
+    if application == 'frontend':
+        remote_filenames = ['/var/apps/frontend/app/views/root/_campaign_notification.html.erb']
+    elif application == 'static':
+        remote_filenames = ["/var/apps/static/app/views/notifications/banner_%s.erb" % i for i in CAMPAIGN_CLASSES]
+    content = ''
+    for remote_filename in remote_filenames:
+        put(StringIO.StringIO(content), remote_filename, use_sudo=True, mirror_local_mode=True)
+        sudo('chown deploy:deploy %s' % remote_filename)
+    execute(app.reload, application)
+
+
+@task
+@roles('class-frontend')
+def deploy_emergency_banner():
+    """Deploy an emergency banner to GOV.UK"""
+    execute(set_context)
+    for application in APPLICATIONS:
+        # Black banners are only placed on the homepage i.e.'frontend'.
+        # Don't deploy a black banner to the static application.
+        if not (env.campaign_class == 'black' and application == 'static'):
+            deploy_banner(application)
+
+@task
+@roles('class-frontend')
+def remove_emergency_banner():
+    """Remove all banners from GOV.UK"""
+    for application in APPLICATIONS:
+        remove_banner(application)
