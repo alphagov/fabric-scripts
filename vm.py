36c8133f492f7a27476753e316ae20aabf082bc4
Alex Muller <alex.muller@digital.cabinet-office.gov.uk>
2016-01-06 10:57:37 +0000

    Remove preview
    Preview is dead, long live integration.

--- vm.py
+++ vm.py
@@ -60,13 +60,7 @@ def bodge_unicorn(name):
     """
     Manually kill off (and restart) unicorn processes by name
 
-    e.g. To kill off and restart contentapi on backend-1 in Preview:
-
-      fab integration -H backend-1.backend vm.bodge_unicorn:contentapi
-
-    ...or on all backend hosts in Preview:
-
-      fab integration class:backend vm.bodge_unicorn:contentapi
+    e.g. vm.bodge_unicorn:contentapi
 
     Yes. This is a bodge. Sorry.
     """
