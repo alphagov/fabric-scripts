24081868781693ba1645d1bce0bad6c580ec52d0
Carlos Vilhena <carlosvilhena@gmail.com>
2016-12-06 15:21:15 +0000

    Use options to avoid typos
    This makes sure people don't mistype the environment.

--- bin/puppet_versions.sh
+++ bin/puppet_versions.sh
@@ -2,7 +2,30 @@
 
 set -e
 
-read -p "Enter the environment: " environment
+PS3='Please enter your choice: '
+options=("integration" "staging" "production" "quit")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "integration")
+            environment="integration"
+            break
+            ;;
+        "staging")
+            environment="staging"
+            break
+            ;;
+        "production")
+            environment="production"
+            break
+            ;;
+        "quit")
+            echo "Exiting..."
+            exit 0
+            ;;
+        *) echo invalid option;;
+    esac
+done
 
 fab $environment all -P -z 10 puppet.config_version | grep -E "\[(.*)\] out: (.+)" | tr -d '[]' | awk -F ' out: ' ' { print $1 "," $2; }' > out.csv
 python bin/show_puppet_versions.py $environment out.csv
