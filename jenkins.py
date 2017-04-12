d5c6a0f3fec1fa807bda08b6abdb606a67186d16
Laura Martin <surminus@gmail.com>
2017-04-12 15:43:03 +0100

    Add jenkins-cli commands
    This may make it easier to do some menial tasks with Jenkins.

---
+++ jenkins.py
@@ -0,0 +1,31 @@
+from fabric.api import sudo, task
+
+
+@task
+def list_plugins():
+    """List installed plugins with friendly name and versions"""
+    sudo('jenkins-cli list-plugins')
+
+
+@task
+def list_plugin_versions():
+    """List installed plugins with current version number"""
+    sudo("jenkins-cli list-plugins |awk '{ if ($NF ~ /\(.*\)/) print $1, $(NF-1); else print $1, $NF }'")
+
+
+@task
+def plugins_requiring_updates():
+    """List plugins requiring an update"""
+    sudo("jenkins-cli |awk '{ if ($NF ~ /\(.*\)/) print $1 }'")
+
+
+@task
+def reload():
+    """Reload configuration from disk"""
+    sudo('jenkins-cli reload-configuration')
+
+
+@task
+def version():
+    """Print the Jenkins version"""
+    sudo('jenkins-cli version')
