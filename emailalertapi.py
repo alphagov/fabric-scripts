949cb7e8ca3c3d7dc21f51c7945190eeb4f481f8
Bevan Loon <bevan.loon@digital.cabinet-office.gov.uk>
2018-02-28 09:47:32 +0000

    Add task to truncate tables in Email Alert Api
    As part of the switch over to using Notify, we want to truncate the new
    tables in Email Alert Api that currently contain only test data. There
    is a rake task to do this in Email Alert Api, but to ease the deploy, we
    want a fab task to call the rake task so we don't have to ssh or rely on
    Jenkins.

--- emailalertapi.py
+++ emailalertapi.py
@@ -7,3 +7,10 @@ import util
 def deliver_test_email(address):
     """Delivers a test email to address"""
     util.rake('email-alert-api', 'deliver:to_test_email', address)
+
+
+@task
+@hosts('email-alert-api-1.backend')
+def truncate_tables():
+    """Truncates tables - ONLY USE ON INITIAL DEPLOY"""
+    util.rake('email-alert-api', 'deploy:truncate_tables')
