d8162270773a97364d93966a790af46a05e8068f
Michael Walker <mike@barrucadu.co.uk>
2019-02-25 13:52:43 +0000

    Abstract over elasticsearch communication

--- elasticsearch.py
+++ elasticsearch.py
@@ -1,12 +1,23 @@
 from fabric.api import abort, run, task
 import re
 
+ELASTICSEARCH_HOST = 'http://localhost:9200'
+
+
+def query_elasticsearch(path, method='GET', host=ELASTICSEARCH_HOST, *args, **kwargs):
+    curl_command = "curl -X{method} '{host}/{path}'".format(
+        method=method,
+        host=host,
+        path=path
+    )
+    return run(curl_command, *args, **kwargs)
+
 
 @task
 def delete(index):
     """Delete an index"""
     if re.match('^[^/]+$', index):
-        run("curl -XDELETE 'http://localhost:9200/%s'" % index)
+        query_elasticsearch(index, method='DELETE')
     else:
         abort("Invalid index provided '%s'" % index)
 
@@ -14,17 +25,16 @@ def delete(index):
 @task
 def status(index):
     """Get the status of an index"""
-    run("curl -XGET 'http://localhost:9200/%s/_status'" % index)
+    query_elasticsearch(index + '/_status')
 
 
 @task
 def cluster_health():
     """Get cluster status"""
-    return run("curl -XGET 'http://localhost:9200/_cluster/health?pretty'",
-               warn_only=True)
+    return query_elasticsearch('/_cluster/health?pretty', warn_only=True)
 
 
 @task
 def check_recovery(index):
     """Check status of an index recovery"""
-    return run("curl -XGET 'http://localhost:9200/{index}/_recovery'".format(index=index))
+    return query_elasticsearch(index + '/_recovery')
